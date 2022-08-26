d4a064c437ddb572c9f829ce655a933c38e86251
JLewis947 <29450687+JLewis947@users.noreply.github.com>
2022-08-26 11:10:16 +0100

    Added stock class for orders to use for demo

--- GranbyChallenge/Stock.cs
+++ GranbyChallenge/Stock.cs
@@ -8,5 +8,37 @@ namespace GranbyChallenge
 {
     public class Stock
     {
+        private int toyStockAmount;
+        private int xboxStockAmount;
+        private int bubblewrapStockAmount;
+        private int cardboardboxStockAmount;
+
+        public Stock(int toyStockAmount, int xboxStockAmount, int bubblewrapStockAmount, int cardboardboxStockAmount)
+        {
+            this.toyStockAmount = toyStockAmount;
+            this.xboxStockAmount = xboxStockAmount;
+            this.bubblewrapStockAmount = bubblewrapStockAmount;
+            this.cardboardboxStockAmount = cardboardboxStockAmount;
+        }
+
+        public int ToyStockAmount
+        {
+            get { return toyStockAmount; }
+        }
+
+        public int XboxStockAmount
+        {
+            get { return xboxStockAmount; }
+        }
+
+        public int BubblewrapStockAmount
+        {
+            get { return bubblewrapStockAmount; }
+        }
+
+        public int CardboardboxStockAmount
+        {
+            get { return cardboardboxStockAmount; }
+        }
     }
 }
